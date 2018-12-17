dffa42fc419d15cb219fc3263ff474ffeaefde8b
LYTWork <45941151+LYTWork@users.noreply.github.com>
2018-12-17 23:15:32 +0800

    Add files via upload

---
+++ 实验五代码版helloworld/UIView.swift
@@ -0,0 +1,24 @@
+//
+//  UIView.swift
+//  实验四4.3代码版helloworld
+//
+//  Created by student on 2018/12/17.
+//  Copyright © 2018年 lyt. All rights reserved.
+//
+
+import UIKit
+
+class UIView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
